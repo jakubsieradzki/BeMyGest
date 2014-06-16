6bdce4c9e77670b081147208e3b9d5fc6e0a4a58
Quepas <oneinchman.dev@gmail.com>
2014-06-16 16:47:26 +0200

    Drawing whole body skeleton markes.

--- DrumMachine/src/main.cpp
+++ DrumMachine/src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <SFML/Graphics.hpp>
 #include "MotionDevice.h"
+#include "HumanTracker.h"
+#include "Util.h"
 
 const int WINDOW_W = 800;
 const int WINDOW_H = 600;
@@ -10,7 +12,9 @@ const std::string WINDOW_NAME = "DrumMachine";
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(WINDOW_W, WINDOW_H), WINDOW_NAME);
-  MotionDevice motion_device(true);
+  MotionDevice motion_device;
+  HumanTracker drummer(XN_SKEL_PROFILE_ALL, motion_device.user_generator());
+  drummer.RegisterCallbacks();
 
   while (window.isOpen())
   {
@@ -23,8 +27,32 @@ int main()
     }
     window.clear();
     // <<<<
+    motion_device.Update();
+    drummer.TrackUsers();
 
-    window.draw(sf::Sprite(motion_device.CaptureImage()));
+    sf::Texture captured_texture = motion_device.CaptureImage();
+    float scale_ratio_x = WINDOW_W/(float)captured_texture.getSize().x;
+    float scale_ratio_y = WINDOW_H/(float)captured_texture.getSize().y;
+
+    sf::Sprite captured_image(captured_texture);
+    captured_image.setScale(scale_ratio_x, scale_ratio_y);
+    window.draw(captured_image);
+
+    std::vector<sf::Vector2f> skeleton_screen_points;
+    Util::ConvertSkeletonMarkersToScreenPoints(
+      motion_device.depth_generator(), 
+      drummer.GetHumanAllPartPosition(drummer.GetFirstTrackedHumanId()), 
+      &skeleton_screen_points);
+
+    // draw marker points for whole skeleton
+    for (unsigned i = 0; i < skeleton_screen_points.size(); ++i)
+    {
+      sf::CircleShape skeleton_marker(5.0f);
+      skeleton_marker.setPosition(
+        skeleton_screen_points[i].x * scale_ratio_x, 
+        skeleton_screen_points[i].y * scale_ratio_y);
+      window.draw(skeleton_marker);
+    }
 
     // >>>>
     window.display();
